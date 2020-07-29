55e352728c0f6157e56d0deb4de97d99937799c1
Rodrigo Garcia Paz <rodrigogarciapaz@122.fibertel.com.ar>
2020-07-29 19:13:50 -0300

    Boton crear pelicula agregado, cada pelicula esta linkeada al detalle de la misma

--- routes/movies.js
+++ routes/movies.js
@@ -7,7 +7,7 @@ router.get("/", moviesController.list);
 router.get('/detail/:id', moviesController.detail);
 router.get('/create', moviesController.create);
 router.post('/create',[
-   check('title').isLength({min:1}).withMessage('Es necesario ingresar como mínimo un caractér'),
+   check('title').isLength({min:1}).withMessage('Es necesario ingresar como mínimo un caracter'),
    check('rating').isInt({min:1, max:10}).withMessage('El rating es entre 1 y 10'),
    check('awards').isInt({min:0}).withMessage('No se pueden ingresar numeros negativos'),
    check('length').isInt().withMessage('Es necesario ingresar un número')
